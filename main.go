5d1d63e0a4afcfe4328947740636edf53e1cf406
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-03-26 16:35:42 +0000

    update main.go with products

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-	books_handler "quickstart/books_handler"
-	dogs_api "quickstart/dogs_api"
+	"quickstart/api/books_api"
+	"quickstart/api/dogs_api"
+	products_api "quickstart/api/store_api"
 	"quickstart/helper"
 
 	"github.com/gorilla/mux"
@@ -15,14 +16,18 @@ func main() {
 	r := mux.NewRouter()
 
 	// Books Routes
-	r.HandleFunc("/api/books", books_handler.GetBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", books_handler.GetBook).Methods("GET")
-	r.HandleFunc("/api/books", books_handler.CreateBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", books_handler.UpdateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", books_handler.DeleteBook).Methods("DELETE")
-
-	// Dog routes
+	r.HandleFunc("/api/books", books_api.GetBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", books_api.GetBook).Methods("GET")
+	r.HandleFunc("/api/books", books_api.CreateBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", books_api.UpdateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", books_api.DeleteBook).Methods("DELETE")
+	
+	// Dog Routes
 	r.HandleFunc("/api/dogs/random", dogs_api.GetRandomDog).Methods("GET")
+	
+	// Store Routes
+	r.HandleFunc("/api/store/products", products_api.GetProducts).Methods("GET")
+	r.HandleFunc("/api/store/product", products_api.CreateProduct).Methods("POST")
 
 	config := helper.GetConfiguration()
 	log.Fatal(http.ListenAndServe(config.Port, r))
