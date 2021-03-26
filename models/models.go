f489db2bfef12238e06a48427e6e62a77fe42128
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-03-26 16:35:27 +0000

    update model with product

--- models/models.go
+++ models/models.go
@@ -1,6 +1,8 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Book model
 type Book struct {
@@ -14,4 +16,15 @@ type Book struct {
 type Author struct {
 	FirstName string `json:"firstname,omitempty" bson:"firstname,omitempty"`
 	LastName  string `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+// Product Model
+type Product struct {
+	ID					primitive.ObjectID	`json:"_id,omitempty" bson:"_id,omitempty"`
+	Title				string							`json:"title,omitempty" bson:"title,omitempty"`
+	Price				string							`json:"price" bson:"price,omitempty"`
+	Category		string							`json:"category" bson:"category,omitempty"`
+	Description	string							`json:"description" bson:"description,omitempty"`
+	Image				string							`json:"image" bson:"image,omitempty"`
+	Uuid				string							`json:"uuid" bson:"uuid,omitempty"`
 }
